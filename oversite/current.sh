fc6fbf23457cb78762c0419d3b526aa4692dbe40
Ken VanDine <ken@vandine.org>
2005-03-04 10:07:28 +0000

    A few housing keeping things

--- oversite/current.sh
+++ oversite/current.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+conary update abiword
+conary update gnome-backgrounds
 /usr/bin/yuck --update --except kernel,xorg-x11,xorg-x11-fonts,xorg-x11-tools,xorg-x11-xfs,conary >> /var/log/oversite 2>&1
