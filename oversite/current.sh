9457824f6fccd0f2843ca01ced071cd65940f3ff
Ken VanDine <ken@vandine.org>
2005-03-08 21:55:10 +0000

    New version

--- oversite/current.sh
+++ oversite/current.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
+#
+# Script used to get Foresight updates
+#
 
-conary update abiword
-conary update gnome-backgrounds
-/usr/bin/yuck --update --except kernel,xorg-x11,xorg-x11-fonts,xorg-x11-tools,xorg-x11-xfs,conary >> /var/log/oversite 2>&1
+# get some variables
+if [ -f /etc/sysconfig/oversite ]; then
+        . /etc/sysconfig/oversite
+fi
+
+if [ !$LOGFILE ]; then
+	LOGFILE=/var/log/oversite
+fi
+
+if [ !$TODO ]; then
+	TODO=/etc/oversite/todo
+fi
+
+if [[ "$AUTO" = yes && -f $TODO ]]
+then
+	for i in `cat /etc/oversite/todo`
+	do
+		conary update $i
+	done
+fi
+
+if [[ "$AUTO" = yes  &&  $EXCEPT ]]
+then
+        /usr/bin/yuck --update --except $EXCEPT >> $LOGFILE 2>&1
+elif [[ "$AUTO" = yes  && !$EXCEPT ]]
+then
+	/usr/bin/yuck --update
+else
+	echo "Auto update is disabled" >> $LOGFILE
+fi
