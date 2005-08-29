c09c88c5fb9b4e5a4ce601d57b2a0dfa0c38a2d8
Gil Hamlin <gil@foresightlinux.com>
2005-08-29 19:40:26 +0000

    Initial build of install epiphany utility

--- install-epiphany/install-epiphany.sh
+++ install-epiphany/install-epiphany.sh
@@ -3,5 +3,17 @@
 #Simple script to fetch epiphany from the repo
 #and then remove the install-epiphany trove
 #
-conary update eiphany
-conary erase install-epiphany
+(
+        echo "33" ; sleep 1
+        conary update eiphany
+        echo "66" ; sleep 1
+        conary erase install-epiphany
+        echo "100" ; sleep 1
+        ) |
+        zenity --progress \
+          --title="Please Wait" \
+          --text="Installing Epiphany..." \
+          --auto-close \
+          --percentage=0
+
+zenity --info --text="Epiphany has been installed and can be found underneath your Applications/Internet menu."
