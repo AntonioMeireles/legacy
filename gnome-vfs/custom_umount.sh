8e5c1333671da2fe6a5f536f0b6a7533eaea72ce
Gil Hamlin <gil@foresightlinux.com>
2005-09-28 19:29:22 +0000

    Bug fixes

--- gnome-vfs/custom_umount.sh
+++ gnome-vfs/custom_umount.sh
@@ -1,5 +1,12 @@
 #! /bin/sh
 DEV=$(grep $1 /etc/mtab |grep -o '^[^" "]*')
-pumount $1
 echo $DEV
-eject $DEV
+#UDI=`lshal 2>/dev/null|grep -B 400 $DEV\' |grep ^udi|tail -1|sed "s/.*'\(.*\)'/\1/"`
+#EJECT=`hal-get-property --udi $UDI --key storage.requires_eject`
+#UMOUNT=`hal-get-property --udi $UDI --key volume.is_mounted`
+#if $UMOUNT; then
+pumount $1
+#fi
+#if $EJECT;then
+#eject $DEV
+#fi
