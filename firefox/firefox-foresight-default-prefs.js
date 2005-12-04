7fbc104b41146ed4de8aeaaac49bf1a47e7d992f
Ken VanDine <ken@vandine.org>
2005-12-04 18:51:58 +0000

    New Version

--- firefox/firefox-foresight-default-prefs.js
+++
@@ -1,5 +0,0 @@
-pref("browser.display.use_system_colors",   true);
-pref("browser.link.open_external",          2);
-pref("general.smoothScroll",                true);
-pref("general.useragent.vendor", "Foresight");
-pref("general.useragent.vendorSub", "FIREFOX_RPM_VR");
