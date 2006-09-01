6ddebf21a645e0836ed44a32431c40054e69c071
Ken VanDine <ken@vandine.org>
2006-09-01 13:14:17 +0000

    Override package selection, remove old cruft

---
+++ anaconda-custom/foresight.py
@@ -0,0 +1,29 @@
+from installclass import BaseInstallClass
+from rhpl.translate import *
+from constants import *
+import os
+import iutil
+
+from autopart import getAutopartitionBoot, autoCreatePartitionRequests
+
+class InstallClass(BaseInstallClass):
+    hidden = 0
+    
+    id = "foresight"
+    name = N_("f_oresight")
+    pixmap = "rpath-color-graphic-only.png"
+    description = N_("Foresight Linux install type.")
+
+    sortPriority = 100
+    showLoginChoice = 1
+
+    def setSteps(self, dispatch):
+        BaseInstallClass.setSteps(self, dispatch);
+        dispatch.skipStep("package-selection")
+        dispatch.skipStep("confirminstall")
+
+    def setGroupSelection(self, grpset, intf):
+        BaseInstallClass.__init__(self, grpset)
+
+        grpset.unselectAll()
+        grpset.selectGroup("everything")
