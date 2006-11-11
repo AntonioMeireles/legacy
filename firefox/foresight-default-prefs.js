047d33511ff3750eddd1a7a886547a7baeddd111
Ken VanDine <ken@vandine.org>
2006-11-11 21:44:03 +0000

    Set some defaults

---
+++ firefox/foresight-default-prefs.js
@@ -0,0 +1,11 @@
+pref("app.update.enabled", false);
+pref("app.update.autoInstallEnabled", false);
+pref("browser.display.use_system_colors",   true);
+pref("browser.link.open_external",          3);
+pref("browser.startup.homepage", "http://www.foresightlinux.org");
+pref("startup.homepage_override_url", "http://www.foresightlinux.org");
+pref("startup.homepage_welcome_url, "http://www.foresightlinux.org");
+pref("general.smoothScroll",                true);
+pref("general.useragent.vendor", "Foreight");
+pref("general.useragent.vendorSub", "FIREFOX_RPM_VR");
+pref("intl.locale.matchOS", true);
