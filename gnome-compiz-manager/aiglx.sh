1e97086f85ec4706627626873870933c1e61bf5c
Antonio Meireles aka doniphon <sbin@reboot.sh>
2007-01-24 12:15:08 +0000

    add check for Composite

--- gnome-compiz-manager/aiglx.sh
+++ gnome-compiz-manager/aiglx.sh
@@ -1,8 +1,14 @@
-#!/bin/bash
+#!/bin/bash                                                                     
 X=`glxinfo | grep GLX_EXT_texture_from_pixmap`
+Y=`xdpyinfo | grep Composite`
 if [ -n "$X" ]; then
-    echo "compiz capable" # put compiz as default wm in gconf
-    /usr/bin/compiz-tray-icon
+    if [ -n "$Y" ]; then
+    
+        echo "compiz capable" # put compiz as default wm in gconf               
+        /usr/bin/compiz-tray-icon
+    else
+        echo "*NOT* compiz capable" # do nothing                                
+    fi
 else
-    echo "*NOT* compiz capable" # do nothing
+    echo "*NOT* compiz capable" # do nothing                                    
 fi
