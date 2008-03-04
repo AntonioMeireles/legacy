97445664184cdcbbce65ba1770de2d8668e1ff1e
Ken VanDine <ken@vandine.org>
2008-03-04 14:53:39 +0000

    promote to :devel

---
+++ xfce-default-session/xfce.csh
@@ -0,0 +1,2 @@
+## This will cause gdm to start xfce as default session
+setenv PREFERRED '/usr/bin/startxfce4'
---
+++ xfce-default-session/xfce.sh
@@ -0,0 +1,2 @@
+## This will cause gdm to start xfce as default session
+export PREFERRED='/usr/bin/startxfce4' 
