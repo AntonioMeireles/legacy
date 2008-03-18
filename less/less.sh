ca2ef3ddd1e2c0ba1c304ad697cc4a5f735be56d
Jeff Uphoff <https://issues.rpath.com/>
2008-03-18 16:03:04 +0000

    eliminate bashisms (RPL-2319)

--- less/less.sh
+++ less/less.sh
@@ -1,5 +1,5 @@
 # less initialization script (sh)
-[ -x /usr/bin/lesspipe.sh ] && export LESSOPEN="|/usr/bin/lesspipe.sh %s"
+[ -x /usr/bin/lesspipe.sh ] && LESSOPEN="|/usr/bin/lesspipe.sh %s" && export LESSOPEN
 
 if [ x"$LC_ALL" != "x" ]; then 
   LANGVAR=$LC_ALL
@@ -8,7 +8,9 @@ else
 fi
 
 if [ x`echo $LANGVAR | cut -b 7- | tr -s "[:lower:]" "[:upper:]"` = x"EUCJP" ]; then
-        export JLESSCHARSET=japanese-euc
+        JLESSCHARSET=japanese-euc
+        export JLESSCHARSET
 elif [ x`echo $LANGVAR | cut -b 7- | tr -s "[:lower:]" "[:upper:]"` = x"EUCKR" ]; then
-        export JLESSCHARSET=korean
+        JLESSCHARSET=korean
+        export JLESSCHARSET
 fi
