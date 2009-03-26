5f4e69697dd1663b1d1b3774885d50eb32b80c2a
Michael K. Johnson <http://issues.rpath.com/>
2009-03-26 14:50:44 +0000

    Update rails and its dependencies to 2.2.2 (RPL-2961)

---
+++ rubygems/rubygems.sh
@@ -0,0 +1,8 @@
+# rubygems requires the following environment variable to work properly
+export GEM_HOME=%(libdir)s/ruby/gems
+
+# add rubygems bin directory to path for gem-generated wrappers
+if ! echo ${PATH} | tr ':' '\n' | grep -q "^${GEM_HOME}/bin$" ; then
+    PATH=$PATH:$GEM_HOME/bin
+fi
+
