6cf14de68aa369f7a6024b83da78f997e0618146
Antonio Meireles aka doniphon <sbin@reboot.sh>
2009-04-02 07:56:11 +0000

    sync with upstream

--- emacs/site-start.el
+++ emacs/site-start.el
@@ -1,6 +1,14 @@
 ;;; loaded before user's ".emacs" file and default.el
 
-;; load ".el" files in "%(datadir)s/%(name)s/site-lisp/site-start.d/" on startup
+;;; load ".el" files in "%(datadir)s/%(name)s/site-lisp/site-start.d/" on startup
 (mapc 'load
       (directory-files "%(datadir)s/%(name)s/site-lisp/site-start.d"
 		       t "\\.el\\'"))
+;;; Emacs can use stock icons in the tool bar when compiled with Gtk+.
+;;; However, this feature is disabled by default.  we enable it...
+(setq icon-map-list '(x-gtk-stock-map))
+
+;;; Defaults conary recipe files to Python mode
+(setq auto-mode-alist
+      (append (list '("\\.recipe\\'" . python-mode))
+              auto-mode-alist))
