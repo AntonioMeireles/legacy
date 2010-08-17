bfa79e28e5b67d5cf089d151f42d1f6a6b0d8d34
Jesse Zhang <jesse@foresightlinux.org>
2010-08-17 06:19:05 +0000

    Update dhcp to 4.x
    The 3.x version doesn't work with new NetworkManager-0.8.1

--- dhcp/findptrsize.c
+++
@@ -1,2 +0,0 @@
-#include <stdio.h>
-int main(void) { printf("%%d\n", sizeof(void *)); return 0; }
--- dhcp/site.h
+++
@@ -1,2 +0,0 @@
-#define _PATH_DHCPD_DB          "%(localstatedir)s/lib/dhcp/dhcpd.leases"
-#define _PATH_DHCLIENT_DB       "%(localstatedir)s/lib/dhcp/dhclient.leases"
