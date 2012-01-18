914149a82a6cf21e0194d6fe485bb4b2e686badb
ermo | Rune Morling <ermo@foresightlinux.org>
2012-01-18 21:01:36 +0000

    Clarify comments.

--- virt-viewer/xauth-libvirt-sdl-hack.sh
+++ virt-viewer/xauth-libvirt-sdl-hack.sh
@@ -5,8 +5,9 @@
 # But since GDM now puts this cookie in a random file in /var/run/gdm, we need
 # to provide a stable symlink in each user's home directory for each session.
 #
-# Note that this will probably break in the corner case where one user
-# logs into an X session on a server running on a second desplay (not :0)
+# Note that this will probably break in the corner case where a user
+# who is already logged into an X session logs into another X session
+# while using the same $HOME directory.
 
 # xhost trickery might also do it (documented here for completeness)
 # xhost +SI:localgroup:kvm
