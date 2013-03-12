4748fa7a68ee02f48dab234a698da95ba8660867
fayek <falk.wittwer@gmail.com>
2013-03-12 06:57:51 +0000

    bump to 24.3

--- emacs/site-start.el
+++ emacs/site-start.el
@@ -1,9 +1,16 @@
 ;;; loaded before user's ".emacs" file and default.el
 
 ;;; load ".el" files in "%(datadir)s/%(name)s/site-lisp/site-start.d/" on startup
-(mapc 'load
-      (directory-files "%(datadir)s/%(name)s/site-lisp/site-start.d"
-		       t "\\.el\\'"))
+;; (mapc 'load
+;;       (directory-files "%(datadir)s/%(name)s/site-lisp/site-start.d"
+;; 		       t "\\.el\\'"))
+(mapc
+ 'load
+ (delete-dups
+  (mapcar 'file-name-sans-extension
+          (directory-files
+           "%(datadir)s/%(name)s/site-lisp/site-start.d" t "\\.elc?\\'"))))
+
 ;;; Emacs can use stock icons in the tool bar when compiled with Gtk+.
 ;;; However, this feature is disabled by default.  we enable it...
 (setq icon-map-list '(x-gtk-stock-map))
