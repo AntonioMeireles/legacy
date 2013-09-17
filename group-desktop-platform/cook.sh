6a082af6c69b193d1efe86869c3aff297231f84d
Antonio Meireles <antonio.meireles@openfiler.com>
2013-09-17 09:35:14 +0000

    add timestamps to group build logs.

--- group-desktop-platform/cook.sh
+++ group-desktop-platform/cook.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 branch=$(grep -m 1 ^branch CONARY)
+date=$(date +%Y.%m.%d-%k:%m.%S)
+touch $date.log
+ln -sf $date.log log 
 case $branch in
   */2-devel)
     BRANCH="foresight.rpath.org@fl:2-devel"
@@ -21,5 +24,5 @@ case $branch in
 esac
 
 #time cvc cook --context $CONTEXT --build-label=$BRANCH group-desktop-platform.recipe'[~!bootstrap,~!builddocs,~!dom0,~!domU,~!vmware,~!xen is: x86]' 2> log2
-time cvc cook --context $CONTEXT --build-label=$BRANCH group-desktop-platform=$BRANCH'[~!bootstrap,~!builddocs,~!dom0,~!domU,~!vmware,~!xen  is: x86 x86_64]' group-desktop-platform=$BRANCH'[~!bootstrap,~!builddocs,~!dom0,~!domU,~!vmware,~!xen is: x86]'  --allow-flavor-change  --debug=all 2>&1 |tee log
-date
+time cvc cook --context $CONTEXT --build-label=$BRANCH group-desktop-platform=$BRANCH'[~!bootstrap,~!builddocs,~!dom0,~!domU,~!vmware,~!xen  is: x86 x86_64]' group-desktop-platform=$BRANCH'[~!bootstrap,~!builddocs,~!dom0,~!domU,~!vmware,~!xen is: x86]'  --allow-flavor-change  --debug=all 2>&1 |tee $date.log
+date +%Y.%m.%d-%k:%m.S
